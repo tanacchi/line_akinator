8993a32e14e5e390f113f538d55ad72972cd6e04
tanacchi <q111026d@mail.kyutech.jp>
2020-01-26 20:22:22 +0900

    Upgrade for Minimum LineBot-FlaskApp

--- tanakinator/__init__.py
+++ tanakinator/__init__.py
@@ -1,5 +1,21 @@
+# Setup for Flask App
 from flask import Flask
 
 app = Flask(__name__)
 
+
+# Setup for LineBot
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+import os
+
+CHANNEL_ACCESS_TOKEN = os.environ['CHANNEL_ACCESS_TOKEN']
+CHANNEL_SECRET       = os.environ['CHANNEL_SECRET']
+
+line    = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+
+# Including other scripts
 import tanakinator.views
--- tanakinator/views.py
+++ tanakinator/views.py
@@ -1,6 +1,31 @@
-from flask import render_template
-from tanakinator import app
+from flask import render_template, request, abort
+from tanakinator import app, line, handler
+
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/line', methods=['POST'])
+def line_webhook():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature.")
+        print("Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line.reply_message(
+        event.reply_token,
+        TextSendMessage(text="Hello."))

f75f3cd7d32f4f17d3b15b16113c8a558f876fc0
tanacchi <q111026d@mail.kyutech.jp>
2020-02-13 19:32:42 +0900

    Remake root page for dynamic features

--- tanakinator/views.py
+++ tanakinator/views.py
@@ -1,9 +1,27 @@
+from collections import defaultdict
+
 from flask import render_template, request, abort
 from tanakinator import app, handler
 
+from tanakinator.akinator import get_feature_table
+
+CIRCLE_CHAR = '&#9675;'
+CROSS_CHAR  = '&#10005;'
+
 @app.route('/')
 def root():
-    return render_template('index.html')
+    feature_table = get_feature_table()
+    table = defaultdict(dict)
+    solutions, questions = {}, {}
+    for solution_id, features in feature_table.items():
+        solutions[solution_id] = solution_id     # FIXME: Use solution name.
+        for question_id, value in features.items():
+            questions[question_id] = question_id # FIXME: Use question name.
+            if value == 1.0:
+                table[solution_id][question_id] = CIRCLE_CHAR
+            else:
+                table[solution_id][question_id] = CROSS_CHAR
+    return render_template('index.html', solutions=solutions, questions=questions, table=table)
 
 @app.route('/line', methods=['POST'])
 def line_webhook():

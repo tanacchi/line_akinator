6ec845c38c2431bf551c0d68f4d2123130d9b2c3
tanacchi <q111026d@mail.kyutech.jp>
2020-02-14 00:59:43 +0900

    Add test of quick message

--- tanakinator/line.py
+++ tanakinator/line.py
@@ -3,11 +3,18 @@ from tanakinator import line, handler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+    QuickReply, QuickReplyButton, MessageAction
 )
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line.reply_message(
-        event.reply_token,
-        TextSendMessage(text="Hello."))
+    if event.message.text == "Play now.":
+        reply_content = TextSendMessage(text="Hello.")
+    else:
+        items = [
+            QuickReplyButton(action=MessageAction(label="Play now", text="Play now."))
+        ]
+        reply_content = TextSendMessage(quick_reply=QuickReply(items=items))
+
+    line.reply_message(event.reply_token, reply_content)

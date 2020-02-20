fbd609e138c7f832196e361b37956051dd203e2f
tanacchi <q111026d@mail.kyutech.jp>
2020-02-21 01:40:57 +0900

    Add test table of database (LatestMessage)

--- tanakinator/models.py
+++ tanakinator/models.py
@@ -1,5 +1,15 @@
 from tanakinator import db
 
 
+class LatestMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(80), nullable=False, unique=True)
+    message = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, user_id, message):
+        self.user_id = user_id
+        self.message = message
+
+
 def init():
     db.create_all()

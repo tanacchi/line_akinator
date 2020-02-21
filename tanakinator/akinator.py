64aad08f0e9a31b817760e1bcec919edb4a1eb55
tanacchi <q111026d@mail.kyutech.jp>
2020-02-22 01:21:46 +0900

    Add GameState enumeration on akinator module

--- tanakinator/akinator.py
+++ tanakinator/akinator.py
@@ -1,3 +1,20 @@
+import enum
+
+
+class GameState(enum.Enum):
+    PENDING     = 'pending'
+    ASKING      = 'asking'
+    GUESSING    = 'guessing'
+    RESUMING    = 'resuming'
+    BEGGING     = 'begging'
+    REGISTERING = 'registering'
+    CONFIRMING  = 'confirming'
+    TRAINING    = 'training'
+    FEATURING   = 'featuring'
+    LABELING    = 'labeling'
+    UPDATING    = 'updating'
+
+
 def get_feature_table():
     result = {
         "S1": {"Q1": 1.0, "Q2": 0.0, "Q3": 1.0},

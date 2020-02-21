d54d9961e63f430a4184dc287f31622c8d9829e9
tanacchi <q111026d@mail.kyutech.jp>
2020-02-22 02:03:22 +0900

    Add test of status switching

--- tanakinator/line.py
+++ tanakinator/line.py
@@ -1,5 +1,6 @@
 from tanakinator import line, handler, db
-from tanakinator.models import LatestMessage
+from tanakinator.models import UserStatus
+from tanakinator.akinator import GameState, get_game_status
 
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
@@ -10,22 +11,24 @@ from linebot.models import (
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
-
     user_id = event.source.user_id
-    latest = db.session.query(LatestMessage).filter_by(user_id=user_id).first()
-    if latest:
-        latest.message = message[:200]
-    else:
-        latest = LatestMessage(user_id, message)
-    db.session.add(latest)
-    db.session.commit()
 
-    if message == "Play now.":
-        reply_content = TextSendMessage(text="Hello.")
-    else:
-        items = [
-            QuickReplyButton(action=MessageAction(label="Play now", text="Play now."))
-        ]
-        reply_content = TextSendMessage(text="Ready?", quick_reply=QuickReply(items=items))
+    reply_text = []
+    if message in [state.value for state in GameState]:
+        current_status = get_game_status(user_id)
+        next_status = GameState(message)
+        reply_text.append(current_status.value + " -> " + next_status.value)
+        user_status = db.session.query(UserStatus).filter_by(user_id=user_id).first()
+        user_status.status = next_status.value
+        db.session.add(user_status)
+        db.session.commit()
+
+    reply_text.append("Choose next game-status.")
+    items = [
+        QuickReplyButton(action=MessageAction(label="PENDING", text="pending")),
+        QuickReplyButton(action=MessageAction(label="ASKING", text="asking")),
+        QuickReplyButton(action=MessageAction(label="GUESSING", text="guessing"))
+    ]
+    reply_content = TextSendMessage(text=reply_text, quick_reply=QuickReply(items=items))
 
     line.reply_message(event.reply_token, reply_content)

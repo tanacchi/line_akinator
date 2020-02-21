903e830bc67ec7a59801f89fac3a85308a48c84b
tanacchi <q111026d@mail.kyutech.jp>
2020-02-22 02:17:30 +0900

    Fix content of multiple reply message

--- tanakinator/line.py
+++ tanakinator/line.py
@@ -13,22 +13,21 @@ def handle_message(event):
     message = event.message.text
     user_id = event.source.user_id
 
-    reply_text = []
+    reply_content = []
     if message in [state.value for state in GameState]:
         current_status = get_game_status(user_id)
         next_status = GameState(message)
-        reply_text.append(current_status.value + " -> " + next_status.value)
+        reply_content.append(current_status.value + " -> " + next_status.value)
         user_status = db.session.query(UserStatus).filter_by(user_id=user_id).first()
         user_status.status = next_status.value
         db.session.add(user_status)
         db.session.commit()
 
-    reply_text.append("Choose next game-status.")
     items = [
         QuickReplyButton(action=MessageAction(label="PENDING", text="pending")),
         QuickReplyButton(action=MessageAction(label="ASKING", text="asking")),
         QuickReplyButton(action=MessageAction(label="GUESSING", text="guessing"))
     ]
-    reply_content = TextSendMessage(text=reply_text, quick_reply=QuickReply(items=items))
+    reply_content.append(TextSendMessage(text="Choose next game-status.", quick_reply=QuickReply(items=items)))
 
     line.reply_message(event.reply_token, reply_content)

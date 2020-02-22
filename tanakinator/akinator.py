4260d6e0017f493cd5773f4ec9386bb711187797
tanacchi <q111026d@mail.kyutech.jp>
2020-02-22 19:42:43 +0900

    Add functions to akinator module for modularity

--- tanakinator/akinator.py
+++ tanakinator/akinator.py
@@ -25,6 +25,47 @@ def get_game_status(user_id):
     status = db.session.query(UserStatus).filter_by(user_id=user_id).first()
     return GameState(status.status) if status else GameState.PENDING
 
+def get_user_status(user_id):
+    user_status = db.session.query(UserStatus).filter_by(user_id=user_id).first()
+    if not user_status:
+        user_status = UserStatus()
+        user_status.user_id = user_id
+        user_status.status = GameState.PENDING.value
+        db.session.add(user_status)
+        db.session.commit()
+    return user_status
+
+def select_next_question(progress):
+    all_questions = Question.query.all()
+    return all_questions[len(progress.answers)]
+
+def save_status(user_status, new_status=None, next_question=None):
+    if new_status:
+        user_status.status = new_status.value
+    if next_question:
+        user_status.latest_question = next_question
+    db.session.add(user_status)
+    db.session.commit()
+
+def can_guess(progress):
+    return len(progress.answers) >= len(Question.query.all())
+
+def push_answer(progress, answer_msg):
+    answer = Answer()
+    answer.question = progress.latest_question
+    answer.value = 1.0 if answer_msg == "はい" else -1.0
+    progress.answers.append(answer)
+    db.session.add(answer)
+    db.session.commit()
+
+def guess_solution(progress):
+    score_table = {s.id: 0.0 for s in Solution.query.all()}
+    for ans in progress.answers:
+        q_features = Feature.query.filter_by(question_id=ans.question_id)
+        for f in q_features:
+            score_table[f.solution.id] += ans.value * f.value
+    return Solution.query.get(max(score_table, key=score_table.get))
+
 def get_feature_table():
     result = {
         "S1": {"Q1": 1.0, "Q2": 0.0, "Q3": 1.0},
--- tanakinator/line.py
+++ tanakinator/line.py
@@ -3,7 +3,11 @@ from tanakinator.models import (
     UserStatus, Question, Progress,
     Answer, Solution, Feature
 )
-from tanakinator.akinator import GameState, get_game_status
+from tanakinator.akinator import (
+    GameState, get_game_status, get_user_status,
+    select_next_question, save_status, can_guess,
+    push_answer, guess_solution
+)
 
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
@@ -17,24 +21,14 @@ def handle_message(event):
     user_id = event.source.user_id
     reply_content = []
 
-    user_status = db.session.query(UserStatus).filter_by(user_id=user_id).first()
-    if not user_status:
-        user_status = UserStatus()
-        user_status.user_id = user_id
-        user_status.status = 'pending'
-        db.session.add(user_status)
-        db.session.commit()
+    user_status = get_user_status(user_id)
     status = GameState(user_status.status)
 
     if status == GameState.PENDING:
         if message == "はじめる":
-            question = Question.query.all()[0]
-            if not user_status.progress:
-                user_status.progress = Progress()
-            user_status.progress.latest_question = question
-            user_status.status = GameState.ASKING.value
-            db.session.add(user_status)
-            db.session.commit()
+            user_status.progress = Progress()
+            question = select_next_question(user_status.progress)
+            save_status(user_status, GameState.ASKING, question)
             items = [
                 QuickReplyButton(action=MessageAction(label="はい", text="はい")),
                 QuickReplyButton(action=MessageAction(label="いいえ", text="いいえ")),
@@ -50,46 +44,34 @@ def handle_message(event):
 
     elif status == GameState.ASKING:
         if message in ["はい", "いいえ"]:
-            answer = Answer()
-            answer.question = user_status.progress.latest_question
-            answer.value = 1.0 if message == "はい" else -1.0
-            user_status.progress.answers.append(answer)
-            if len(user_status.progress.answers) < len(Question.query.all()):
-                question = Question.query.all()[len(user_status.progress.answers)]
-                user_status.progress.latest_question = question
+            push_answer(user_status.progress, message)
+            if not can_guess(user_status.progress):
+                question = select_next_question(user_status.progress)
+                save_status(user_status, next_question=question)
                 items = [
                     QuickReplyButton(action=MessageAction(label="はい", text="はい")),
                     QuickReplyButton(action=MessageAction(label="いいえ", text="いいえ")),
                 ]
                 reply_content.append(TextSendMessage(text=question.message, quick_reply=QuickReply(items=items)))
             else:
-                score_table = {s.id: 0.0 for s in Solution.query.all()}
-                for ans in user_status.progress.answers:
-                    q_features = db.session.query(Feature).filter_by(question_id=ans.question.id)
-                    for f in q_features:
-                        score_table[f.solution.id] += ans.value * f.value
-                most_likely_solution = Solution.query.get(max(score_table, key=score_table.get))
+                most_likely_solution = guess_solution(user_status.progress)
                 reply_text = "思い浮かべているのは\n\n" + most_likely_solution.name + "\n\nですか?"
                 items = [
                     QuickReplyButton(action=MessageAction(label="はい", text="はい")),
                     QuickReplyButton(action=MessageAction(label="いいえ", text="いいえ")),
                 ]
                 reply_content.append(TextSendMessage(text=reply_text, quick_reply=QuickReply(items=items)))
-                user_status.status = GameState.GUESSING.value
-            db.session.add(user_status)
-            db.session.commit()
+                save_status(user_status, GameState.GUESSING)
         else:
             reply_text = "Pardon?"
             reply_content.append(TextSendMessage(text=reply_text))
 
     elif status == GameState.GUESSING:
         if message in ["はい", "いいえ"]:
-            user_status.status = GameState.PENDING.value
             reply_text = "やったー" if message == "はい" else "ええ〜"
             db.session.query(Answer).filter_by(progress_id=user_status.progress.id).delete()
             db.session.delete(user_status.progress)
-            db.session.add(user_status)
-            db.session.commit()
+            save_status(user_status, GameState.PENDING)
         else:
             reply_text = "Pardon?"
         reply_content.append(TextSendMessage(text=reply_text))

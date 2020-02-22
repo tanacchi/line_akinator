757d669b96e9cca7097a9e24656b4354e9b3db79
tanacchi <q111026d@mail.kyutech.jp>
2020-02-22 15:42:12 +0900

    Add test function of guessing process

--- tanakinator/akinator.py
+++ tanakinator/akinator.py
@@ -1,6 +1,9 @@
 import enum
 
-from tanakinator.models import UserStatus
+from tanakinator.models import (
+    UserStatus, Question, Answer,
+    Solution, Feature
+)
 from tanakinator import db
 
 
@@ -30,3 +33,32 @@ def get_feature_table():
         "S4": {"Q1": 1.0, "Q2": 0.0, "Q3": 0.0},
     }
     return result
+
+
+def test():
+    answers = []
+    all_questions = Question.query.all()
+    print(all_questions)
+
+    while len(answers) < len(all_questions):
+        q = all_questions[len(answers)]
+        ans_msg = input(q.message + "[y/n]")
+        if not ans_msg in ['y', 'n']:
+            raise RuntimeError("Invalid answer.")
+        answer = Answer()
+        answer.question = q
+        answer.value = 1.0 if ans_msg == 'y' else -1.0
+        answers.append(answer)
+        db.session.add(answer)
+        db.session.commit()
+        print(answers)
+
+        score_table = {s.id: 0.0 for s in Solution.query.all()}
+        for ans in answers:
+            q_features = db.session.query(Feature).filter_by(question_id=ans.question.id)
+            for f in q_features:
+                score_table[f.solution.id] += ans.value * f.value
+        print(score_table)
+
+    most_likely_solution = Solution.query.get(max(score_table, key=score_table.get))
+    print(most_likely_solution.name)

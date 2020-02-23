e0132e74bc3e5b5f2537d4afac62c734b4eab3cc
tanacchi <q111026d@mail.kyutech.jp>
2020-02-23 13:17:55 +0900

    Add page to add new solution

--- tanakinator/views.py
+++ tanakinator/views.py
@@ -5,11 +5,17 @@ from flask import (
     abort, redirect, url_for
 )
 from tanakinator import app, handler, db
-from tanakinator.models import Solution, Question
+from tanakinator.models import Solution, Question, Feature
 from tanakinator.akinator import get_feature_table
 
 CIRCLE_CHAR = '&#9675;'
 CROSS_CHAR  = '&#10005;'
+
+str_value_table = {
+    "はい":    1.0,
+    None:      0.0,
+    "いいえ": -1.0
+}
 
 @app.route('/')
 def root():
@@ -22,6 +28,27 @@ def root():
             table[s_id][q_id] = CIRCLE_CHAR if value == 1.0 else CROSS_CHAR
     return render_template('index.html', solutions=solutions, questions=questions, table=table)
 
+@app.route('/solutions/create', methods=['GET', 'POST'])
+def solution_create():
+    if request.method == 'GET':
+        questions = Question.query.all()
+        return render_template('solutions/create.html', questions=questions)
+    else:
+        new_solution = Solution()
+        new_solution.name = request.form.get("name")
+        for attr, value in request.form.items():
+            if attr == 'name':
+                new_solution.name = value
+            else:
+                q_id = int(attr[2:])
+                feature = Feature()
+                feature.question_id = q_id
+                feature.value = str_value_table[request.form.get(f"q_{q_id}")]
+                new_solution.features.append(feature)
+        db.session.add(new_solution)
+        db.session.commit()
+        return redirect(url_for('root'))
+
 @app.route('/solutions/<int:solution_id>/edit', methods=['GET', 'POST'])
 def solution_edit(solution_id):
     solution = Solution.query.get(solution_id)
@@ -31,8 +58,7 @@ def solution_edit(solution_id):
         return render_template('solutions/edit.html', **kwargs)
     else:
         for f in solution.features:
-            value_str = request.form.get(f"q_{f.question_id}")
-            f.value = 1.0 if value_str == "はい" else -1.0
+            f.value = str_value_table[request.form.get(f"q_{f.question_id}")]
         db.session.add(solution)
         db.session.commit()
         return redirect(url_for('root'))

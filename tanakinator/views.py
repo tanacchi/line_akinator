97f8a125ea913820318a3a3d42dc1ccaf6181252
tanacchi <q111026d@mail.kyutech.jp>
2020-02-23 12:25:48 +0900

    Upgrade views to edit features on browser

--- tanakinator/views.py
+++ tanakinator/views.py
@@ -1,7 +1,10 @@
 from collections import defaultdict
 
-from flask import render_template, request, abort
-from tanakinator import app, handler
+from flask import (
+    render_template, request,
+    abort, redirect, url_for
+)
+from tanakinator import app, handler, db
 from tanakinator.models import Solution, Question
 from tanakinator.akinator import get_feature_table
 
@@ -19,12 +22,20 @@ def root():
             table[s_id][q_id] = CIRCLE_CHAR if value == 1.0 else CROSS_CHAR
     return render_template('index.html', solutions=solutions, questions=questions, table=table)
 
-@app.route('/solutions/<int:solution_id>/edit')
+@app.route('/solutions/<int:solution_id>/edit', methods=['GET', 'POST'])
 def solution_edit(solution_id):
     solution = Solution.query.get(solution_id)
-    feature_table = {f: Question.query.get(f.question_id) for f in solution.features}
-    kwargs = {'solution': solution, 'feature_table': feature_table}
-    return render_template('solutions/edit.html', **kwargs)
+    if request.method == 'GET':
+        feature_table = {f: Question.query.get(f.question_id) for f in solution.features}
+        kwargs = {'solution': solution, 'feature_table': feature_table}
+        return render_template('solutions/edit.html', **kwargs)
+    else:
+        for f in solution.features:
+            value_str = request.form.get(f"q_{f.question_id}")
+            f.value = 1.0 if value_str == "はい" else -1.0
+        db.session.add(solution)
+        db.session.commit()
+        return redirect(url_for('root'))
 
 
 from linebot.exceptions import InvalidSignatureError

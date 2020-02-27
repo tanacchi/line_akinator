f0ae41a7ac7da4673402a5bbba0335d12b0ef75c
tanacchi <q111026d@mail.kyutech.jp>
2020-02-27 22:33:39 +0900

    Set secret_key of Flask application core

--- tanakinator/__init__.py
+++ tanakinator/__init__.py
@@ -1,11 +1,12 @@
 # Setup for Flask App
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = os.environ['TANAKINATOR_SECRET_KEY']
 
 
 # Setup for Database
-import os
 from flask_sqlalchemy import SQLAlchemy
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///local.db'
